805805ee7794204e5e7a4b9a291daa1e75a45725
Walter Cervini <wcervini@gmail.com>
2026-01-08 02:53:35 +0000

    Se añade el plugin de copilot Se modifica init.lua eliminando la todo lo de mason

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,20 @@
+-- Con lazy.nvim
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<M-l>",
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
+      },
+      panel = { enabled = false },
+    })
+  end,
+}
